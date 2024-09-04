c1dd20fa98e8d974923146353c974a1441c9e498
swaranjit banerjee <swaranjitbanerjee7@gmail.com>
2024-09-04 23:14:04 +0530

    commited-2

--- chat-app/firebase.js
+++ chat-app/firebase.js
@@ -1,11 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-//auth import -> step 1
-import {getAuth} from "firebase/auth";
-// firestore import -> step 1
-import {getFirestore} from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-
+import { getAnalytics } from "firebase/analytics"
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -13,22 +8,15 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD0_-m-3GN1m712tv4kWcoN6GmUj-rQDuw",
-  authDomain: "chat-app-4730e.firebaseapp.com",
-  projectId: "chat-app-4730e",
-  storageBucket: "chat-app-4730e.appspot.com",
-  messagingSenderId: "479235362830",
-  appId: "1:479235362830:web:e6e6f48a838c684009ec5f",
-  measurementId: "G-QTTLTCNC94"
+  apiKey: "AIzaSyDLKwtfBF8p-5zrpBI7LJpKk1I4oji-Yg8",
+  authDomain: "chattify-45c29.firebaseapp.com",
+  projectId: "chattify-45c29",
+  storageBucket: "chattify-45c29.appspot.com",
+  messagingSenderId: "802189501823",
+  appId: "1:802189501823:web:1d0836a1abb3db1a6fe64a",
+  measurementId: "G-5YPN8NF39F"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// auth step 2
-const auth = getAuth(app);
-// firestore step 2
-const db = getFirestore();
-const storage = getStorage();
-
-
-export { auth, db, storage };
+const analytics = getAnalytics(app);
